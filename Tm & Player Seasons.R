67c5eee49fe332d050712b33518965fae72bcc6b
Sumitro Datta <sumitrodatta@gmail.com>
2020-09-18 17:24:17 -0400

    initialize w/ 1947-2020 regular season data

---
+++ Tm & Player Seasons.R
@@ -0,0 +1,259 @@
+library(rvest)
+library(tidyverse)
+library(janitor)
+
+#team-stats-base
+#opponent-stats-base
+#team-stats-per_poss from 1974
+#opponent-stats-per_poss from 1974
+teamStats<-function(season=2020,league="NBA",type="team-stats-base"){
+  url=paste0("https://www.basketball-reference.com/leagues/",league,"_",season,".html")
+  new_season=url %>% read_html %>% html_nodes(xpath = '//comment()') %>%
+    html_text() %>% paste(collapse='') %>% read_html() %>% 
+    html_nodes(xpath=paste0('//*[(@id = "div_',type,'")]')) %>% 
+    html_nodes("table") %>% .[[1]] %>% html_table()
+  new_season<-new_season %>% rename(Season=Rk) %>% mutate(Season=season) %>% 
+    add_column(Lg=league,.before="Team") %>% clean_names()
+  return(new_season)
+}
+
+abbrev<-read_csv("Team Abbrev.csv")
+
+get_all_team_stats<-function(to_scrape="team-stats-base")
+{
+  a<-teamStats(2020,type=to_scrape)
+  if (to_scrape %in% c("team-stats-per_poss","opponent-stats-per_poss"))
+  {
+    #nba
+    sapply(2019:1974,function(x){
+      new_seas<-teamStats(x,type=to_scrape)
+      a<<-rbind(a,new_seas)
+    })
+    #aba
+    sapply(1976:1974,function(x){
+      new_seas<-teamStats(x,"ABA",type=to_scrape)
+      a<<-rbind(a,new_seas)
+    })
+  }
+  else
+  {
+    #nba
+    sapply(2019:1950,function(x){
+      new_seas<-teamStats(x,type=to_scrape)
+      a<<-rbind(a,new_seas)
+    })
+    #aba
+    sapply(1976:1968,function(x){
+      new_seas<-teamStats(x,"ABA",type=to_scrape)
+      a<<-rbind(a,new_seas)
+    })
+    #baa
+    sapply(1949:1947,function(x){
+      new_seas<-teamStats(x,"BAA",type=to_scrape)
+      a<<-rbind(a,new_seas)
+    })
+  }
+  a<- a %>%
+    mutate(playoffs=str_detect(team,"\\*"),
+           team=ifelse(playoffs==TRUE,substr(team,1,nchar(team)-1),team)) %>%
+    relocate(playoffs,.after="team")
+  a<-full_join(a,abbrev)
+  a<-a %>% relocate(abbreviation,.after="team")
+  return(a)
+}
+
+team_stats_base<-get_all_team_stats()
+
+opp_stats_base<-get_all_team_stats("opponent-stats-base")
+opp_stats_base<-opp_stats_base %>% rename_at(vars(-c(1:7)),~paste0("opp_",.))
+
+team_stats_pg<-get_all_team_stats("team-stats-per_game")
+team_stats_pg<-team_stats_pg %>%
+  rename_at(vars(-c(1:6,10,13,16,19)),~paste0(.,"_per_game"))
+
+opp_stats_pg<-get_all_team_stats("opponent-stats-per_game")
+opp_stats_pg<-opp_stats_pg %>%
+  rename_at(vars(-c(1:7)),~paste0("opp_",.)) %>%
+  rename_at(vars(-c(1:6,10,13,16,19)),~paste0(.,"_per_game"))
+
+team_stats_per_poss<-get_all_team_stats("team-stats-per_poss")
+team_stats_per_poss<-team_stats_per_poss %>% 
+  rename_at(vars(-c(1:7,10,13,16,19)),~paste0(.,"_per_100_poss"))
+
+opp_stats_per_poss<-get_all_team_stats("opponent-stats-per_poss")
+opp_stats_per_poss<-opp_stats_per_poss %>% 
+  rename_at(vars(-c(1:7)),~paste0("opp_",.)) %>%
+  rename_at(vars(-c(1:7,10,13,16,19)),~paste0(.,"_per_100_poss"))
+
+misc_stats_scrape<-function(season=2020,league="NBA"){
+  url=paste0("https://www.basketball-reference.com/leagues/",league,"_",season,".html")
+  new_season=url %>% read_html %>% html_nodes(xpath = '//comment()') %>%
+    html_text() %>% paste(collapse='') %>% read_html() %>% 
+    html_nodes(xpath='//*[(@id = "div_misc_stats")]') %>% 
+    html_nodes("table") %>% .[[1]] %>% html_table()
+  new_season[1,22:25]=paste0("opp_",new_season[1,22:25])
+  colnames(new_season)<-new_season[1,]
+  new_season<-new_season[-1,]
+  new_season[,c(1,3:25)]<-sapply(new_season[,c(1,3:25)],as.numeric)
+  new_season<-new_season %>% rename(Season=Rk) %>% mutate(Season=season) %>% 
+    add_column(Lg=league,.before="Team") %>% 
+    mutate(Playoffs=str_detect(Team,"\\*"),
+           Team=ifelse(Playoffs==TRUE,substr(Team,1,nchar(Team)-1),Team)) %>%
+    relocate(Playoffs,.after="Team") %>% clean_names()
+  new_season<-left_join(new_season,abbrev)
+  new_season<-new_season %>% relocate(abbreviation,.after="team")
+  return(new_season)
+}
+
+tm_summaries<-misc_stats_scrape()
+sapply(2019:1950,function(x){
+  new_seas=misc_stats_scrape(x)
+  tm_summaries<<-rbind(tm_summaries,new_seas)
+})
+sapply(1949:1947,function(x){
+  new_seas=misc_stats_scrape(x,"BAA")
+  tm_summaries<<-rbind(tm_summaries,new_seas)
+})
+sapply(1976:1968,function(x){
+  new_seas=misc_stats_scrape(x,"ABA")
+  tm_summaries<<-rbind(tm_summaries,new_seas)
+})
+
+write_excel_csv(tm_summaries,"Team Summaries.csv")
+write_excel_csv(opp_stats_per_poss,"Opponent Stats Per 100 Poss.csv")
+write_excel_csv(team_stats_per_poss,"Team Stats Per 100 Poss.csv")
+write_excel_csv(team_stats_base,"Team Totals.csv")
+write_excel_csv(opp_stats_base,"Opponent Totals.csv")
+write_excel_csv(team_stats_pg,"Team Stats Per Game.csv")
+write_excel_csv(opp_stats_pg,"Opponent Stats Per Game.csv")
+
+all_team<-left_join(tm_summaries,team_stats_base) %>% 
+  left_join(.,team_stats_per_poss) %>% left_join(.,opp_stats_base) %>% 
+  left_join(.,opp_stats_per_poss)
+
+scrape_stats <- function(season = 2017,league="NBA",type="totals"){
+  #scrape
+  url <- paste0("https://www.basketball-reference.com/leagues/",league,"_",season,"_",type,".html")
+  stats_a <- url %>% read_html() %>% html_table() %>% .[[1]]
+  
+  #clean
+  player_stats_a <- stats_a %>% clean_names() %>% 
+    filter(!player=="Player") %>%
+    mutate_at(vars(-c(player,tm,pos)),as.numeric) %>% 
+    as_tibble() %>% 
+    mutate(rk=season) %>% rename(Season=rk) %>% mutate(Lg=league,.before="tm") %>% clean_names()
+  return(player_stats_a)
+}
+
+get_all<-function(to_scrape="totals")
+{
+  a<-scrape_stats(2020,type=to_scrape)
+  if (to_scrape=="per_poss")
+  {
+    #nba
+    sapply(2019:1974,function(x){
+      new_seas<-scrape_stats(x,type=to_scrape)
+      names(new_seas)<-names(a)
+      a<<-rbind(a,new_seas)
+    })
+    #aba
+    sapply(1976:1974,function(x){
+      new_seas<-scrape_stats(x,"ABA",type=to_scrape)
+      names(new_seas)<-names(a)
+      a<<-rbind(a,new_seas)
+    })
+  }
+  else
+  {
+      #nba
+      sapply(2019:1950,function(x){
+        new_seas<-scrape_stats(x,type=to_scrape)
+        names(new_seas)<-names(a)
+        a<<-rbind(a,new_seas)
+      })
+      #aba
+      sapply(1976:1968,function(x){
+        new_seas<-scrape_stats(x,"ABA",type=to_scrape)
+        names(new_seas)<-names(a)
+        a<<-rbind(a,new_seas)
+      })
+      #baa
+      sapply(1949:1947,function(x){
+        new_seas<-scrape_stats(x,"BAA",type=to_scrape)
+        names(new_seas)<-names(a)
+        a<<-rbind(a,new_seas)
+      })
+  }
+  #removed asterisk from hall of fame players and add as separate column
+  a<-a %>% 
+    mutate(hof=str_detect(player,"\\*"),
+           player=ifelse(hof==TRUE,substr(player,1,nchar(player)-1),player)) %>%
+    left_join(.,read_csv("Player Season Info.csv"))
+  a<-a %>% relocate(seas_id,season,player_id,player,birth_year,hof,pos,age,experience,lg)
+  return(a)
+}
+totals<-get_all()
+
+per_game<-get_all("per_game")
+per_game<-per_game %>% rename_at(vars(-c(1:13,17,20,23:24,27)),~paste0(.,"_per_game"))
+
+advanced<-get_all("advanced")
+advanced<-advanced %>% select(-c(x,x_2))
+
+per_36<-get_all("per_minute")
+per_36<-per_36 %>% rename_at(vars(-c(1:14,17,20,23,26)),~paste0(.,"_per_36_min"))
+
+per_100<-get_all("per_poss")
+per_100<-per_100 %>% select(-x) %>% filter(season>1973)
+per_100<-per_100 %>% rename_at(vars(-c(1:14,17,20,23,26,36:37)),~paste0(.,"_per_100_poss"))
+
+all_play<-left_join(totals,per_game) %>% 
+  left_join(.,per_36) %>% left_join(.,per_100) %>% left_join(.,advanced)
+
+write_excel_csv(advanced,"Advanced.csv")
+write_excel_csv(per_100,"Per 100 Poss.csv")
+write_excel_csv(per_36,"Per 36 Minutes.csv")
+write_excel_csv(totals,"Player Totals.csv")
+write_excel_csv(per_game,"Player Per Game.csv")
+
+#player season info starts with season, player, pos, age, tm, league, hof
+#go into excel and add birth years for following player careers:
+#players with same name in same season (example: George Johnson)
+#players with same name in different seasons (example: Gerald Henderson)
+#players who played seasons in multiple leagues (example: Moses Malone)
+
+#create season ids
+a<-totals %>% select(season:tm) %>% mutate(hof=ifelse(is.na(hof),FALSE,hof)) %>% 
+  arrange(season,player) %>% mutate(seas_id=row_number())
+#create player ids
+b<-a %>% group_by(player,birth_year) %>% 
+  slice(1) %>% arrange(seas_id) %>% ungroup() %>% mutate(player_id=row_number()) %>% 
+  select(player,birth_year,player_id)
+a<-left_join(a %>% select(-player_id),b)
+#add years of experience
+a<-a %>% mutate(tm=ifelse(tm=="TOT","1TOT",tm))
+b<-a %>% group_by(player_id,season) %>% arrange(tm) %>% slice(1) %>% 
+  ungroup() %>% group_by(player_id) %>% mutate(experience=row_number())
+a<-left_join(a %>% select(-experience),b) %>% arrange(player_id,season,tm) %>% 
+  fill(experience) %>% mutate(tm=ifelse(tm=="1TOT","TOT",tm)) %>% arrange(seas_id) %>% 
+  relocate(seas_id,player_id,.after="season")
+write_excel_csv(a,"Player Season Info.csv")
+
+# add_new_seas(season=2021,type="totals"){
+#   a<-scrape_stats(season=season,type=type)
+#   a<-a %>% 
+#     mutate(hof=FALSE,birth_year=NA) %>% full_join(.,read_csv("Player Season Info.csv"))
+#   a<-a %>% relocate(seas_id,season,player_id,player,birth_year,hof,pos,age,experience,lg)
+#   a<-a %>% arrange(season,player) %>% 
+#     mutate(seas_id=ifelse(is.na(seas_id),max(seas_id)+cumsum(is.na(seas_id)),seas_id))
+#   b<-a %>% group_by(player,birth_year) %>% 
+#     slice(1) %>% arrange(seas_id) %>% ungroup() %>% 
+#     mutate(player_id=ifelse(is.na(player_id),max(player_id)+cumsum(is.na(player_id)),player_id))
+#   a<-full_join(a,b)
+#   a<-a %>% mutate(tm=ifelse(tm=="TOT","1TOT",tm))
+#   b<-a %>% group_by(player_id,season) %>% arrange(tm) %>% slice(1) %>% 
+#     ungroup() %>% ungroup() %>% group_by(player_id) %>% mutate(experience=row_number())
+#   a<-full_join(a,b) %>% arrange(player_id,season,tm) %>% fill(experience) %>% 
+#     mutate(tm=ifelse(tm=="1TOT","TOT",tm)) %>% arrange(seas_id)
+#   a<-a %>% filter(season == season)
+# }
---
+++ award shares.R
@@ -0,0 +1,215 @@
+library(rvest)
+library(tidyverse)
+library(janitor)
+
+#works with mvp, roy
+get_award_pcts_mvp_roy<-function(season=2020,award="mvp",lg="nba")
+{
+  url=paste0('https://www.basketball-reference.com/awards/awards_',season,'.html')
+  awarding=award
+  #aba mvp awarded from 1968-1976
+  if (award=="mvp")
+  {
+    if (season<1977 & season>1967){
+      awarding=paste0(lg,"_",award)
+    }
+  }
+  else #aba roy has voting results from 1972 to 1975 (other years added later) 
+  {
+    if (season<1976 & season>1971){
+      awarding=paste0(lg,"_",award)
+    }
+  }
+  pcts =  url %>% read_html %>%
+  html_nodes(xpath = paste0('//*[(@id = "div_',awarding,'")]')) %>% .[[1]] %>% html_node("table") %>% html_table()
+  pcts<-pcts[,2:8] #get only voting results (no stats)
+  colnames(pcts)<-pcts[1,] #first row is actual column names
+  pcts<-pcts[-1,]
+  rownames(pcts)<-c() #reset row names
+  #add season awarded and award name
+  pcts<-pcts %>% add_column(Season=season,.before="Player") %>% 
+    add_column(Award=paste(lg,award),.before="Player") %>% 
+    add_column(Winner=FALSE) %>% clean_names()
+  return(pcts)
+}
+#add nba mvps
+mvp<-get_award_pcts_mvp_roy()
+sapply(2019:1956, function(x){
+  new_seas<-get_award_pcts_mvp_roy(x,"mvp")
+  mvp<<-rbind(mvp,new_seas)
+})
+
+#add aba mvps
+sapply(1976:1968, function(x){
+  new_seas<-get_award_pcts_mvp_roy(x,"mvp","aba")
+  mvp<<-rbind(mvp,new_seas)
+})
+
+#add nba roys
+roy<-get_award_pcts_mvp_roy(award = "roy")
+sapply(c(2019:1964,1959), function(x){
+  new_seas<-get_award_pcts_mvp_roy(x,"roy")
+  roy<<-rbind(roy,new_seas)
+})
+
+#add aba roys
+sapply(1975:1972, function(x){
+  new_seas<-get_award_pcts_mvp_roy(x,"roy","aba")
+  roy<<-rbind(roy,new_seas)
+})
+
+#add nba roy winners with no voting results
+roys<- "https://www.basketball-reference.com/awards/roy.html" %>% read_html %>% 
+  html_nodes(xpath='//*[(@id = "div_roy_NBA")]') %>% .[[1]] %>% html_node("table") %>% html_table()
+colnames(roys)<-roys[1,]
+roys<-roys[-1,]
+roys<-roys %>% filter(Voting != "(V)") %>% mutate(Season=as.numeric(substr(Season,0,4))+1)
+roys<-roys %>% select(-c(Lg,Voting,G:`WS/48`)) %>% add_column(Award="nba roy",.before="Player") %>% 
+  add_column("First"=NA,"Pts Won"=NA,"Pts Max"=NA,"Share"=NA,"Winner"=TRUE) %>% clean_names()
+roy<-rbind(roy,roys)
+
+#add aba roy winners with no voting results
+roys<- "https://www.basketball-reference.com/awards/roy.html" %>% read_html %>% 
+  html_nodes(xpath='//*[(@id = "div_roy_ABA")]') %>% .[[1]] %>% html_node("table") %>% html_table()
+colnames(roys)<-roys[1,]
+roys<-roys[-1,]
+roys<-roys %>% filter(Voting != "(V)") %>% mutate(Season=as.numeric(substr(Season,0,4))+1)
+#1971 was a tie, so get rid of trailing (Tie) remark for Charlie Scott and Dan Issel
+roys<-roys %>% mutate(Player=ifelse(str_detect(Player,"(Tie)"),substr(Player,1,nchar(Player)-6),Player))
+roys<-roys %>% select(-c(Lg,Voting,G:`WS/48`)) %>% add_column(Award="aba roy",.before="Player") %>% 
+  add_column("First"=NA,"Pts Won"=NA,"Pts Max"=NA,"Share"=NA,"Winner"=TRUE) %>% clean_names()
+roy<-rbind(roy,roys)
+
+rm(roys)
+
+#works with dpoy, smoy, mip (all there are nba-only awards)
+get_award_pcts_other<-function(season=2020,award="mip"){
+  url=paste0('https://www.basketball-reference.com/awards/awards_',season,'.html')
+  pcts =  url %>% read_html %>% html_nodes(xpath = '//comment()') %>% 
+    html_text() %>% paste(collapse='') %>% read_html() %>% 
+    html_node(paste0("#",award)) %>% html_table()
+  pcts<-pcts[,2:8] #get only voting results (no stats)
+  colnames(pcts)<-pcts[1,] #first row is actual column names
+  pcts<-pcts[-1,]
+  rownames(pcts)<-c() #reset row names
+  #add season awarded and award name
+  pcts<-pcts %>% add_column(Season=season,.before="Player") %>% 
+    add_column(Award=award,.before="Player") %>% mutate(winner=FALSE) %>%
+    clean_names()
+  return(pcts)
+}
+mip<-get_award_pcts_other(2020,"mip")
+seasons=c(2019:1988,1986) #no voting found for 1987
+sapply(seasons, function(x){
+  new_seas<-get_award_pcts_other(x,"mip")
+  mip<<-rbind(mip,new_seas)
+})
+mip<-mip %>% add_row(season=1987,award="mip",player="Dale Ellis",age="26",tm="SEA",winner=TRUE)
+
+dpoy<-get_award_pcts_other(2020,"dpoy")
+sapply(2019:1983, function(x){
+  new_seas<-get_award_pcts_other(x,"dpoy")
+  dpoy<<-rbind(dpoy,new_seas)
+})
+
+smoy<-get_award_pcts_other(2020,"smoy")
+seasons=2019:1984 #no voting found for 1983
+sapply(seasons, function(x){
+  new_seas<-get_award_pcts_other(x,"smoy")
+  smoy<<-rbind(smoy,new_seas)
+})
+smoy<-smoy %>% add_row(season=1983,award="smoy",player="Bobby Jones",age="31",tm="PHI",winner=TRUE)
+
+awards<-rbind(dpoy,smoy,mip,mvp,roy)
+awards[,c(1,4,6:9)]<-sapply(awards[,c(1,4,6:9)],as.numeric)
+
+winners<-awards %>% group_by(season,award) %>% slice_max(share)
+awards<-anti_join(awards,winners)
+winners<-winners %>% mutate(winner=TRUE)
+awards<-full_join(awards,winners) %>% arrange(desc(season),award,desc(share))
+psi<-read_csv("Player Season Info.csv")
+awards<-awards %>% 
+  mutate (player=ifelse(player=="Jaren Jackson" & season >= 2019,"Jaren Jackson Jr.",player)) %>%
+  mutate (player=ifelse(player=="Marvin Bagley" & season >= 2019,"Marvin Bagley III",player)) %>%
+  mutate (player=ifelse(player=="Dennis Smith" & season >= 2018,"Dennis Smith Jr.",player)) %>%
+  mutate (player=ifelse(player=="Taurean Waller-Prince" & season >= 2018,"Taurean Prince",player)) %>%
+  mutate (player=ifelse(player=="Tim Hardaway" & season >= 2014,"Tim Hardaway Jr.",player)) %>%
+  mutate (player=ifelse(player=="Nenê Hilário" & season >= 2003,"Nenê",player))
+  
+awards<- left_join(awards,psi) %>% relocate(seas_id,player_id,.before=player) %>% 
+  relocate(birth_year,pos,.before=age) %>% relocate(experience,.after=age) %>% select(-c(hof,lg))
+
+write_excel_csv(awards,"Player Award Shares.csv")
+
+#get all-league teams
+all_lg<-"https://www.basketball-reference.com/awards/all_league.html" %>% read_html %>%
+  html_nodes(xpath='//*[(@id = "div_awards_all_league")]') %>% html_node("table") %>% html_table() %>% .[[1]]
+#change blanks to NA's and remove
+all_lg[all_lg==""]=NA
+all_lg<-na.omit(all_lg)
+colnames(all_lg)<-c("Season","Lg","team_rank","Player1","Player2","Player3","Player4","Player5")
+all_lg<-all_lg %>% mutate(Season=as.numeric(substr(Season,0,4))+1)
+#players become one column
+all_lg<-all_lg %>% pivot_longer(-c(Season,Lg,team_rank),names_prefix = "Player") %>% select(-name)
+#separate out ties
+all_lg<-all_lg %>% rename("Player"=value) %>% separate_rows(Player,sep=", ")
+all_lg<-all_lg %>% mutate(Player=ifelse(str_detect(Player,"\\(T\\)$"),substr(Player,1,nchar(Player)-4),str_trim(Player)))
+#separate out positions
+nopos<-all_lg %>% filter(!str_detect(Player,"(C|F|G)$")) %>% mutate(Position=NA)
+all_lg<-all_lg %>% filter(str_detect(Player,"(C|F|G)$")) %>% 
+       mutate(Position=str_sub(Player,start=-1),Player=substr(Player,1,nchar(Player)-2))
+all_lg<-rbind(all_lg,nopos)
+all_lg<-all_lg %>% mutate(Type=paste("All",Lg,sep="-")) %>% select(1,2,6,3,4,5) %>% clean_names()
+
+all_def_or_all_rookie<-function(type="all_defense")
+{
+  url<-paste0("https://www.basketball-reference.com/awards/",type,".html")
+  alldef<-url %>% read_html %>%
+    html_nodes(xpath=paste0('//*[(@id = "div_awards_',type,'")]')) %>% 
+    html_node("table") %>% html_table() %>% .[[1]]
+  alldef[alldef==""]=NA
+  alldef<-alldef[complete.cases(alldef),]
+  colnames(alldef)<-c("Season","Lg","team_rank","Player1","Player2","Player3","Player4","Player5")
+  alldef<-alldef %>% mutate(Season=as.numeric(substr(Season,0,4))+1)
+  alldef<-alldef %>% pivot_longer(-c(Season,Lg,team_rank),names_prefix = "Player") %>% select(-name)
+  alldef<-alldef %>% rename("Player"=value) %>% separate_rows(Player,sep=", ")
+  tieds<-alldef %>% 
+    filter(str_detect(Player,"\\(T\\)$")) %>% mutate(Player=substr(Player,1,nchar(Player)-4))
+  alldef<-alldef %>% filter(!str_detect(Player,"\\(T\\)$"))
+  alldef<-rbind(alldef,tieds) %>% arrange(desc(Season),Lg,team_rank)
+  if (type=="all_defense")
+  {
+    alldef<-alldef %>% add_column(Type="All-Defense",.before="team_rank")
+  }
+  else
+  {
+    alldef<-alldef %>% add_column(Type="All-Rookie",.before="team_rank")
+  }
+  alldef<-alldef %>% mutate(Position=NA) %>% clean_names()
+  return(alldef)
+}
+alldef<-all_def_or_all_rookie()
+allrook<-all_def_or_all_rookie("all_rookie")
+
+end_seas_teams<-rbind(all_lg,alldef,allrook)
+
+psi<-read_csv("Player Season Info.csv")
+end_seas_teams<-end_seas_teams %>% 
+  mutate (player=ifelse(player=="Jaren Jackson" & season >= 2019,"Jaren Jackson Jr.",player)) %>%
+  mutate (player=ifelse(player=="Marvin Bagley" & season >= 2019,"Marvin Bagley III",player)) %>%
+  mutate (player=ifelse(player=="Dennis Smith" & season >= 2018,"Dennis Smith Jr.",player)) %>%
+  mutate (player=ifelse(player=="Tim Hardaway" & season >= 2014,"Tim Hardaway Jr.",player)) %>%
+  mutate (player=ifelse(player=="Nenê Hilário" & season >= 2003,"Nenê",player))
+end_seas_teams<-left_join(end_seas_teams,psi) %>% group_by(season,type,player) %>% 
+  mutate(n=n()) %>% select(season:birth_year,tm,age,n)
+#two George Johnsons played at same time, one won All-Defense, remove the other
+end_seas_teams<-end_seas_teams %>% filter(!(player=="George Johnson" & birth_year==1956))
+#if player played for multiple teams during season, keep only total stat
+tots<-end_seas_teams %>% filter(n>1 & player != "George Johnson")
+end_seas_teams<-anti_join(end_seas_teams,tots)
+tots<-tots %>% mutate(tm=ifelse(tm=="TOT","1TOT",tm)) %>% 
+  group_by(player_id,type) %>% arrange(tm) %>% slice(1) %>% 
+  mutate(tm=ifelse(tm=="1TOT","TOT",tm))
+end_seas_teams<-full_join(end_seas_teams,tots) %>% select(-n) %>% arrange(desc(season),type,team_rank)
+
+write_csv(end_seas_teams,"End of Season Teams.csv")
