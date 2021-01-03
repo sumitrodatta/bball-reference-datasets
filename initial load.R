75efcef3a4e2f9769b60023524366d94556be4db
Sumitro Datta <sumitrodatta@gmail.com>
2021-01-03 12:25:59 -0500

    separate scripts
    all functions remain in Tm&Player Seasons
    
    initial load and adding a new season transferred to their own scripts for readability

---
+++ initial load.R
@@ -0,0 +1,96 @@
+library(rvest)
+library(tidyverse)
+library(janitor)
+
+source("Tm & Player Seasons.R")
+
+team_stats_base<-get_all_team_stats()
+
+opp_stats_base<-get_all_team_stats("opponent-stats-base")
+opp_stats_base<-opp_stats_base %>% rename_at(vars(-c(1:7)),~paste0("opp_",.))
+
+team_stats_pg<-get_all_team_stats("team-stats-per_game")
+team_stats_pg<-team_stats_pg %>%
+  rename_at(vars(-c(1:6,10,13,16,19)),~paste0(.,"_per_game"))
+
+opp_stats_pg<-get_all_team_stats("opponent-stats-per_game")
+opp_stats_pg<-opp_stats_pg %>%
+  rename_at(vars(-c(1:7)),~paste0("opp_",.)) %>%
+  rename_at(vars(-c(1:6,10,13,16,19)),~paste0(.,"_per_game"))
+
+team_stats_per_poss<-get_all_team_stats("team-stats-per_poss")
+team_stats_per_poss<-team_stats_per_poss %>% 
+  rename_at(vars(-c(1:7,10,13,16,19)),~paste0(.,"_per_100_poss"))
+
+opp_stats_per_poss<-get_all_team_stats("opponent-stats-per_poss")
+opp_stats_per_poss<-opp_stats_per_poss %>% 
+  rename_at(vars(-c(1:7)),~paste0("opp_",.)) %>%
+  rename_at(vars(-c(1:7,10,13,16,19)),~paste0(.,"_per_100_poss"))
+
+tm_summaries<-misc_stats_scrape()
+sapply(2019:1950,function(x){
+  new_seas=misc_stats_scrape(x)
+  tm_summaries<<-rbind(tm_summaries,new_seas)
+})
+sapply(1949:1947,function(x){
+  new_seas=misc_stats_scrape(x,"BAA")
+  tm_summaries<<-rbind(tm_summaries,new_seas)
+})
+sapply(1976:1968,function(x){
+  new_seas=misc_stats_scrape(x,"ABA")
+  tm_summaries<<-rbind(tm_summaries,new_seas)
+})
+tm_summaries=left_join(tm_summaries,read_csv("Team Abbrev.csv")) %>% 
+  relocate(c(abbreviation,playoffs),.after="team")
+
+write_excel_csv(tm_summaries,"Team Summaries.csv")
+write_excel_csv(opp_stats_per_poss,"Opponent Stats Per 100 Poss.csv")
+write_excel_csv(team_stats_per_poss,"Team Stats Per 100 Poss.csv")
+write_excel_csv(team_stats_base,"Team Totals.csv")
+write_excel_csv(opp_stats_base,"Opponent Totals.csv")
+write_excel_csv(team_stats_pg,"Team Stats Per Game.csv")
+write_excel_csv(opp_stats_pg,"Opponent Stats Per Game.csv")
+
+totals<-get_all()
+
+per_game<-get_all("per_game")
+per_game<-per_game %>% rename_at(vars(-c(1:13,17,20,23:24,27)),~paste0(.,"_per_game"))
+
+advanced<-get_all("advanced")
+advanced<-advanced %>% select(-c(x,x_2))
+
+per_36<-get_all("per_minute")
+per_36<-per_36 %>% rename_at(vars(-c(1:14,17,20,23,26)),~paste0(.,"_per_36_min"))
+
+per_100<-get_all("per_poss")
+per_100<-per_100 %>% select(-x) %>% filter(season>1973)
+per_100<-per_100 %>% rename_at(vars(-c(1:14,17,20,23,26,36:37)),~paste0(.,"_per_100_poss"))
+
+write_excel_csv(advanced,"Advanced.csv")
+write_excel_csv(per_100,"Per 100 Poss.csv")
+write_excel_csv(per_36,"Per 36 Minutes.csv")
+write_excel_csv(totals,"Player Totals.csv")
+write_excel_csv(per_game,"Player Per Game.csv")
+
+#player season info starts with season, player, pos, age, tm, league, hof
+#go into excel and add birth years for following player careers:
+#players with same name in same season (example: George Johnson)
+#players with same name in different seasons (example: Gerald Henderson)
+#players who played seasons in multiple leagues (example: Moses Malone)
+
+#for updating past seasons
+totals_info=totals %>% select(season,player:tm) %>% select(-experience) %>%
+  arrange(season,player) %>% mutate(seas_id=row_number())
+players_unique<-totals_info %>% group_by(player,birth_year) %>% 
+  slice(1) %>% arrange(seas_id) %>% ungroup() %>% 
+  mutate(player_id=row_number()) %>% select(player,birth_year,player_id)
+totals_info<-left_join(totals_info,players_unique)
+totals_info<-totals_info %>% mutate(tm=ifelse(tm=="TOT","1TOT",tm))
+removed_mult_same_yr<-totals_info %>% group_by(player_id,season) %>% 
+  arrange(tm) %>% slice(1) %>% ungroup() %>% group_by(player_id) %>% 
+  mutate(experience=row_number())
+totals_info<-left_join(totals_info,removed_mult_same_yr) %>% 
+  arrange(player_id,season,tm) %>% fill(experience) %>% 
+  mutate(tm=ifelse(tm=="1TOT","TOT",tm)) %>% arrange(seas_id) %>% 
+  relocate(seas_id,player_id,.after="season")
+write_csv(totals_info,"Player Season Info.csv")
---
+++ new season update.R
@@ -0,0 +1,139 @@
+library(rvest)
+library(tidyverse)
+library(janitor)
+
+source("Tm & Player Seasons.R")
+
+add_new_team_seas<-function(seas=2021,type="team-stats-base",update_abbrevs=FALSE){
+  if (type=="misc"){
+    a <-misc_stats_scrape(season=seas)
+  }
+  else {
+    a <- teamStats(season=seas,type=type)
+  }
+  if (update_abbrevs==TRUE){
+    abbrev=read_csv("Team Abbrev.csv") %>% filter (season != seas)
+    new_seas=a %>% select(season:team) %>%
+      mutate(playoffs=str_detect(team,"\\*"),
+             team=ifelse(playoffs==TRUE,substr(team,1,nchar(team)-1),team))
+    previous_seas=abbrev %>% filter (season == seas-1) %>% select(team,abbreviation)
+    new_seas=left_join(new_seas,previous_seas) %>% arrange(team)
+    abbrev=abbrev %>% add_row(new_seas)
+    write_csv(abbrev,"Team Abbrev.csv")
+  }
+  a<-left_join(a,read_csv("Team Abbrev.csv")) %>% 
+    relocate(c(abbreviation,playoffs),.after="team")
+  if (type=="misc"){
+    old=read_csv("Team Summaries.csv") %>% filter(season != seas)
+    a<-a %>% mutate(attend=gsub(",","",attend),attend_g=gsub(",","",attend_g)) %>%
+      mutate(across(c(attend,attend_g),as.numeric))
+    write_csv(old %>% add_row(a),"Team Summaries.csv")
+  }
+  else if (type=="team-stats-base"){
+    old=read_csv("Team Totals.csv") %>% filter(season != seas)
+    write_csv(old %>% add_row(a),"Team Totals.csv")
+  }
+  else if (type=="opponent-stats-base"){
+    old=read_csv("Opponent Totals.csv") %>% filter(season != seas)
+    a<-a %>% rename_at(vars(-c(1:7)),~paste0("opp_",.))
+    write_csv(old %>% add_row(a),"Opponent Totals.csv")
+  }
+  else if (type=="team-stats-per_game"){
+    old=read_csv("Team Stats Per Game.csv") %>% filter(season != seas)
+    a<-a %>%
+      rename_at(vars(-c(1:6,10,13,16,19)),~paste0(.,"_per_game"))
+    write_csv(old %>% add_row(a),"Team Stats Per Game.csv")
+  }
+  else if (type=="opponent-stats-per_game"){
+    old=read_csv("Opponent Stats Per Game.csv") %>% filter(season != seas)
+    a<-a %>%
+      rename_at(vars(-c(1:7)),~paste0("opp_",.)) %>%
+      rename_at(vars(-c(1:6,10,13,16,19)),~paste0(.,"_per_game"))
+    write_csv(old %>% add_row(a),"Opponent Stats Per Game.csv")
+  }
+  else if (type=="team-stats-per_poss"){
+    old=read_csv("Team Stats Per 100 Poss.csv") %>% filter(season != seas)
+    a<-a %>% 
+      rename_at(vars(-c(1:7,10,13,16,19)),~paste0(.,"_per_100_poss"))
+    write_csv(old %>% add_row(a),"Team Stats Per 100 Poss.csv")
+  }
+  else if (type=="opponent-stats-per_poss"){
+    old=read_csv("Opponent Stats Per 100 Poss.csv") %>% filter(season != seas)
+    a<-a %>% 
+      rename_at(vars(-c(1:7)),~paste0("opp_",.)) %>%
+      rename_at(vars(-c(1:7,10,13,16,19)),~paste0(.,"_per_100_poss"))
+    write_csv(old %>% add_row(a),"Opponent Stats Per 100 Poss.csv")
+  }
+}
+
+add_new_seas<-function(seas=2021,type="totals",update_psi=FALSE){
+  a<-scrape_stats(season=seas,type=type)
+  if (update_psi==TRUE){
+    # no active hall of famers
+    new_player_info=a %>% select(season:tm) %>% mutate(hof=FALSE) %>%
+      arrange(season,player)
+    psi=read_csv("Player Season Info.csv") %>% 
+      select(season,player:tm) %>% 
+      filter(season != seas)
+    updated_psi=psi %>% add_row(new_player_info)
+    #season ids
+    updated_psi=updated_psi %>% mutate(seas_id=row_number())
+    players_unique<-updated_psi %>% group_by(player,birth_year) %>% 
+      slice(1) %>% arrange(seas_id) %>% ungroup() %>% 
+      mutate(player_id=row_number()) %>% select(player,birth_year,player_id)
+    updated_psi<-left_join(updated_psi,players_unique)
+    #add years of experience
+    updated_psi<-updated_psi %>% mutate(tm=ifelse(tm=="TOT","1TOT",tm))
+    removed_mult_same_yr<-updated_psi %>% group_by(player_id,season) %>% 
+      arrange(tm) %>% slice(1) %>% ungroup() %>% group_by(player_id) %>% 
+      mutate(experience=row_number())
+    updated_psi<-left_join(updated_psi,removed_mult_same_yr) %>% 
+      arrange(player_id,season,tm) %>% fill(experience) %>% 
+      mutate(tm=ifelse(tm=="1TOT","TOT",tm)) %>% arrange(seas_id) %>% 
+      relocate(seas_id,player_id,.after="season")
+    write_csv(updated_psi,"Player Season Info.csv")
+  }
+  a<-left_join(a,read_csv("Player Season Info.csv")) %>% 
+    relocate(seas_id,season,player_id,player,birth_year,hof,pos,age,experience,lg)
+  if (type=="totals"){
+    old=read_csv("Player Totals.csv") %>% filter(season != seas)
+    write_csv(old %>% add_row(a),"Player Totals.csv")
+  }
+  else if (type=="advanced"){
+    old=read_csv("Advanced.csv") %>% filter(season != seas)
+    a<-a %>% select(-c(x,x_2))
+    write_csv(old %>% add_row(a),"Advanced.csv")
+  }
+  else if (type=="per_game"){
+    old=read_csv("Player Per Game.csv") %>% filter(season != seas)
+    a<-a %>% rename_at(vars(-c(1:13,17,20,23:24,27)),~paste0(.,"_per_game"))
+    write_csv(old %>% add_row(a),"Player Per Game.csv")
+  }
+  else if (type=="per_minute"){
+    old=read_csv("Per 36 Minutes.csv") %>% filter(season != seas)
+    a<-a %>% rename_at(vars(-c(1:14,17,20,23,26)),~paste0(.,"_per_36_min"))
+    write_csv(old %>% add_row(a),"Per 36 Minutes.csv")
+  }
+  else if (type=="per_poss"){
+    old=read_csv("Per 100 Poss.csv") %>% filter(season != seas)
+    a<-a %>% select(-x) %>% 
+      rename_at(vars(-c(1:14,17,20,23,26,36:37)),~paste0(.,"_per_100_poss"))
+    write_csv(old %>% add_row(a),"Per 100 Poss.csv")
+  }
+}
+
+add_new_team_seas(seas=2021,type="team-stats-base",update_abbrevs = TRUE)
+add_new_team_seas(seas=2021,type="opponent-stats-base",update_abbrevs = FALSE)
+add_new_team_seas(seas=2021,type="team-stats-per_game",update_abbrevs = FALSE)
+add_new_team_seas(seas=2021,type="opponent-stats-per_game",update_abbrevs = FALSE)
+add_new_team_seas(seas=2021,type="team-stats-per_poss",update_abbrevs = FALSE)
+add_new_team_seas(seas=2021,type="opponent-stats-per_poss",update_abbrevs = FALSE)
+add_new_team_seas(seas=2021,type="misc",update_abbrevs = FALSE)
+
+add_new_seas(seas=2021,type="totals",update_psi=TRUE)
+add_new_seas(seas=2021,type="advanced",update_psi=FALSE)
+add_new_seas(seas=2021,type="per_game",update_psi=FALSE)
+add_new_seas(seas=2021,type="per_minute",update_psi=FALSE)
+add_new_seas(seas=2021,type="per_poss",update_psi=FALSE)
+
+
