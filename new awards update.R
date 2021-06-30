751293360ba3c5d5a3cf17f0212736019788e4bc
Sumitro Datta <sumitrodatta@gmail.com>
2021-06-29 20:56:31 -0400

    add 2021 end-of-season teams + awards
    2021 all-league teams (defense, nba, rookie)
    
    award shares for all 5 player awards (dpoy, mip, mvp, roy, smoy)
    
    upgrade awards update script to use year variable to minimize changes for future years
    
    add all-defense, all-league, all-rook scrape & rowbind to previous file

--- new awards update.R
+++ new awards update.R
@@ -5,11 +5,12 @@ library(polite)
 
 source("award shares.R")
 
-mvp <- get_award_pcts_mvp_roy(season = 2021, award = "mvp")
-roy <- get_award_pcts_mvp_roy(season = 2021, award = "roy")
-mip <- get_award_pcts_other(season = 2021, award = "mip")
-dpoy <- get_award_pcts_other(season = 2021, award = "mip")
-smoy <- get_award_pcts_other(season = 2021, award = "smoy")
+curr_year=2021
+mvp <- get_award_pcts_mvp_roy(season = curr_year, award = "mvp")
+roy <- get_award_pcts_mvp_roy(season = curr_year, award = "roy")
+mip <- get_award_pcts_other(season = curr_year, award = "mip")
+dpoy <- get_award_pcts_other(season = curr_year, award = "dpoy")
+smoy <- get_award_pcts_other(season = curr_year, award = "smoy")
 
 new_seas_awards <- rbind(dpoy, smoy, mip, mvp, roy)
 
@@ -17,18 +18,23 @@ new_seas_awards[, c(1, 4, 6:9)] <- sapply(new_seas_awards[, c(1, 4, 6:9)], as.nu
 
 winners <- new_seas_awards %>%
   group_by(award) %>%
-  slice_max(share)
+  slice_max(share) %>% ungroup()
 new_seas_awards <- anti_join(new_seas_awards, winners)
 winners <- winners %>% mutate(winner = TRUE)
 new_seas_awards <- full_join(new_seas_awards, winners) %>% arrange(award, desc(share))
 psi <- read_csv("Player Season Info.csv")
 new_seas_awards <- new_seas_awards %>%
-  mutate(player = ifelse(player == "Jaren Jackson" & season >= 2019, "Jaren Jackson Jr.", player)) %>%
-  mutate(player = ifelse(player == "Marvin Bagley" & season >= 2019, "Marvin Bagley III", player)) %>%
-  mutate(player = ifelse(player == "Dennis Smith" & season >= 2018, "Dennis Smith Jr.", player)) %>%
-  mutate(player = ifelse(player == "Taurean Waller-Prince" & season >= 2018, "Taurean Prince", player)) %>%
-  mutate(player = ifelse(player == "Tim Hardaway" & season >= 2014, "Tim Hardaway Jr.", player)) %>%
-  mutate(player = ifelse(player == "Nenê Hilário" & season >= 2003, "Nenê", player))
+  mutate(player=case_when(
+    (player == "Jaren Jackson" & season >=2019)~"Jaren Jackson Jr.",
+    (player == "Marvin Bagley" & season >= 2019)~"Marvin Bagley III",
+    (player == "Dennis Smith" & season >= 2018)~"Dennis Smith Jr.",
+    (player == "Taurean Waller-Prince" & season >= 2018)~"Taurean Prince",
+    (player == "Tim Hardaway" & season >= 2014)~"Tim Hardaway Jr.",
+    (player == "Nenê Hilário" & season >= 2003)~"Nenê",
+    (player == "Michael Porter" & season >= 2020)~"Michael Porter Jr.",
+    TRUE~player
+  )
+  )
 
 new_seas_awards <- left_join(new_seas_awards, psi) %>%
   relocate(seas_id, player_id, .before = player) %>%
@@ -37,9 +43,12 @@ new_seas_awards <- left_join(new_seas_awards, psi) %>%
   select(-c(hof, lg))
 
 old_awards <- read_csv("Player Award Shares.csv")
-write_csv(old_awards %>% add_row(new_seas_awards), "Player Award Shares.csv")
+write_csv(old_awards %>% 
+            filter(season != curr_year) %>% 
+            add_row(new_seas_awards) %>%
+            arrange(desc(season),award,desc(share)), "Player Award Shares.csv")
 
-new_seas_allstars <- all_stars(season = 2021) %>%
+new_seas_allstars <- all_stars(season = curr_year) %>%
   mutate(
     replaced = str_detect(player, "\\("),
     player = ifelse(replaced == TRUE, substr(player, 1, nchar(player) - 4), player),
@@ -47,4 +56,28 @@ new_seas_allstars <- all_stars(season = 2021) %>%
     player = ifelse(hof == TRUE, substr(player, 1, nchar(player) - 1), player)
   )
 
-write_csv(read_csv("All-Star Selections.csv") %>% add_row(new_seas_allstars), "All-Star Selections.csv")
+write_csv(read_csv("All-Star Selections.csv") %>% filter(season != curr_year) %>%
+            add_row(new_seas_allstars), "All-Star Selections.csv")
+
+all_lg <- all_lg_scrape()
+alldef <- all_def_or_all_rookie()
+allrook <- all_def_or_all_rookie("all_rookie")
+
+new_end_seas_teams=bind_rows(all_lg,alldef,allrook) %>% filter(season==curr_year) %>%
+  mutate(player=case_when(
+    (player == "Jaren Jackson" & season >=2019)~"Jaren Jackson Jr.",
+    (player == "Marvin Bagley" & season >= 2019)~"Marvin Bagley III",
+    (player == "Dennis Smith" & season >= 2018)~"Dennis Smith Jr.",
+    (player == "Taurean Waller-Prince" & season >= 2018)~"Taurean Prince",
+    (player == "Tim Hardaway" & season >= 2014)~"Tim Hardaway Jr.",
+    (player == "Nenê Hilário" & season >= 2003)~"Nenê",
+    (player == "Michael Porter" & season >= 2020)~"Michael Porter Jr.",
+    TRUE~player
+  )
+  ) %>% left_join(.,psi) %>% select(season:birth_year,tm,age)
+
+write_csv(read_csv("End of Season Teams.csv") %>% 
+            filter(season != curr_year) %>%
+            add_row(new_end_seas_teams) %>% 
+            arrange(desc(season), type, team_rank),
+          "End of Season Teams.csv")
